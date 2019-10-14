726cce6b0db078006bccb4038ace29f8480a4cf9
z <d7fhx69@gmail.com>
2019-10-14 09:13:42 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Ciao, to get started, edit <code>src/App.js</code> and save to reload.
+          This code was changed on the feature branch by edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
